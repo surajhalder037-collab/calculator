a2c34fd9b8563ad0c3069ade5dc6b0241874486f
suraj <surajhalder037@gmail.com>
2026-09-09 22:52:37 +0530

    style: update calculator UI with improved button styles and layout adjustments

--- @latesd/src/App.jsx
+++ @latesd/src/App.jsx
@@ -17,7 +17,7 @@ function Cal() {
   ];
 
   let operators = [
-    "+", "−", "×", "÷", "%"
+    "+", "-", "*", "/", "%"
   ];
 
   let actions = [
@@ -57,33 +57,6 @@ function Cal() {
           return <button onClick={() => { setDisplayValue([...displayValue, number]) }} key={number}>{number}</button>;
         })}
 
-
-
-
-
-        {/* <button class="clear">AC</button>
-        <button></button> */}
-        {/* <button>%</button>
-      <button class="operator">÷</button>
-
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button class="operator">×</button>
-
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button class="operator">−</button>
-
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button class="operator">+</button>
-
-      <button class="zero">0</button>
-      <button>.</button>
-      <button class="equal">=</button> */}
       </div>
 
     </div >
